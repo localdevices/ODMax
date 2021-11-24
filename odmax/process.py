7ea573333f186429d3e8198f36f7517a819c6af6
hcwinsemius <h.c.winsemius@gmail.com>
2021-11-24 10:57:54 +0100

    docstrings for overlap #14

--- odmax/process.py
+++ odmax/process.py
@@ -8,7 +8,7 @@ def reproject_cube(img, **kwargs):
     :param img: ndarray with dimensions [H, W, 3] containing image
     :param face_w: int defining the length of each face of the cube (default: 256)
     :param mode: str defining the reprojection mode, can be 'bilinear' or 'nearest'
-    :param kwargs: keyword arguments to pass to py360convert library
+    :param overlap: float, defining the amount of overlap on face edges defined as ratio of face_w (e.g. 0.1). If not set, this will default to 0.1
     :return: list of ndarrays in shape of [H, W, 3] containing images of cube faces
     """
     assert (isinstance(img, np.ndarray)), "provided img is not a numpy array"
