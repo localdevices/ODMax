6a01d0aa0ce279fe93ed8de786db7a191ac5c613
hcwinsemius <h.c.winsemius@gmail.com>
2021-12-01 08:47:59 +0100

    bug fix #18

--- odmax/process.py
+++ odmax/process.py
@@ -15,7 +15,9 @@ def reproject_cube(img, **kwargs):
     assert (isinstance(img, np.ndarray)), "provided img is not a numpy array"
     if not "overlap" in kwargs:
         kwargs["overlap"] = 0.1  # always default to 0.1
-    if not("face_w" in kwargs):
+    print(kwargs)
+    # if not("face_w" in kwargs):
+    if kwargs["face_w"] is None:
         # if kwargs["face_w"] is None:
         face_w_no_overlap = int(img.shape[1]/4)  # a quarter of the width of the still
         face_w = int(face_w_no_overlap * (1 + 2 * kwargs["overlap"]))  # add twice the overlap to the face width
